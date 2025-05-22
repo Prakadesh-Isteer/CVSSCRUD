9dcef3a35d3ff730b77b0786d84424ee6831fd23
Prakadesh-Isteer <prakadesh.u@isteer.com>
2025-05-22 15:01:36 +0530

    Security config

---
+++ src/main/java/com/example/isteer/configuration/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.isteer.configuration;
+
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+     CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:4200"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+     CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
+    }
+
+  
+}
